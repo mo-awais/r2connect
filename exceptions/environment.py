cafc0e70b9333467994ac27520e5f624c0056854
Mohammed Awais <awais@mohammedawais.me>
2023-09-04 21:16:01 +0100

    Remove redundant code

--- exceptions/environment.py
+++
@@ -1,8 +0,0 @@
-class ConfigNotFound(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-
-
-class ConfigAlreadyExists(Exception):
-    def __init__(self, message):
-        super().__init__(message)
