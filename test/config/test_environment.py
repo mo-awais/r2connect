c9866f4d7535509fa55b7f01709e24cc8e71bb35
Mohammed Awais <awais@mohammedawais.me>
2023-09-04 20:18:28 +0100

    Update test

--- test/config/test_environment.py
+++ test/config/test_environment.py
@@ -5,15 +5,17 @@ from exceptions.environment import ConfigNotFound, ConfigAlreadyExists
 
 
 class TestEnvironment(TestCase):
-    config = EnvironmentConfig()
+    config = EnvironmentConfig(environment="test")
 
     def test_get(self):
-        self.assertEquals("TEST", self.config.get("ENVIRONMENT"))
+        self.assertEqual("TEST", self.config.get("ENVIRONMENT"))
 
     def test_get_exception(self):
         expected_exception = ConfigNotFound("The following config does not exist: UNITTEST")
 
         with self.assertRaises(ConfigNotFound) as actual_exception:
             self.config.get("UNITTEST")
+
+            self.assertEqual(expected_exception, actual_exception)
 
         print(actual_exception.exception)
