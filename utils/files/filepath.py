4f275c6d9ba149a18b9f3098913a2371cb64a255
Mohammed Awais <mohammed.awais@boohoo.com>
2023-09-08 11:50:40 +0100

    Create test for filepath utils

---
+++ utils/files/filepath.py
@@ -0,0 +1,22 @@
+import re
+
+
+class Filepath:
+    @staticmethod
+    def safe_filepath(unsafe_filepath: str) -> str:
+        """
+        Use regex to strip illegal characters from a string filepath
+
+        :param unsafe_filepath: A filepath string containing illegal characters
+        :returns: A filepath string without any illegal characters
+        """
+
+        return re.sub(
+            r"'",
+            "",
+            re.sub(
+                r"[/\\?%*:;&#<>$`!{}@*= |\"<>\x7F\x00-\x1F]",
+                "-",
+                unsafe_filepath
+            )
+        )
