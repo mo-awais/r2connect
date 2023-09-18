2a66be7c27f3e4b758691aff08c0cf5299278aac
Mohammed Awais <mohammed.awais@boohoo.com>
2023-09-18 16:09:31 +0100

    Add receipts table

--- server/models.py
+++ server/models.py
@@ -7,3 +7,11 @@ class User(UserMixin, db.Model):
     id = db.Column(db.String(100), primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+
+
+class Receipts(db.Model):
+    id = db.Column(db.String(100), primary_key=True)
+    company = db.Column(db.String(100))
+    item_name = db.Column(db.String(100))
+    purchase_date = db.Column(db.String(100))
+    price = db.Column(db.String(100))
